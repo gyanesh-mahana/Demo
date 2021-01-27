1ae06b4c1d7ebadb824c190ab4128d798d684ab0
gyanesh <gyanesh@gyanesh-ZED-Air-CX3>
2021-01-27 17:19:16 +0530

    updated deploy shell script

--- JenkinsScriptProject/deploy.sh
+++ JenkinsScriptProject/deploy.sh
@@ -1,1 +1,5 @@
-deploy adapters: [tomcat9(credentialsId: '6c41928c-4b3f-4672-ba81-43ccfe0d40e5', path: '', url: 'http://localhost:8081')], contextPath: 'scriptjenkinswebapp_2', war: '**/*.war'
+#
+#	Deploy the war file to tomcat9
+#
+
+mvn deploy -DaltDeploymentRepository=script-repo::default::https://tomcat:tomcat@localhost:8081
