a2c94a7972afd4e20663f87fdfdb002af2c804be
Gyanesh <gkm010@gmail.com>
2021-01-27 17:35:21 +0530

    replaced mvn command with jenkins snippet

--- JenkinsScriptProject/deploy.sh
+++ JenkinsScriptProject/deploy.sh
@@ -2,4 +2,5 @@
 #	Deploy the war file to tomcat9
 #
 
-mvn deploy -DaltDeploymentRepository=script-repo::default::https://tomcat:tomcat@localhost:8081
+#mvn deploy -DaltDeploymentRepository=script-repo::default::https://tomcat:tomcat@localhost:8081
+deploy adapters: [tomcat9(credentialsId: '6c41928c-4b3f-4672-ba81-43ccfe0d40e5', path: '', url: 'http://localhost:8081/')], contextPath: 'deployedfromscriptwebapp', war: '**/*.war'
