c9fe09249a1cb80d67547d87f37cddb496cdcdd3
Gyanesh <gkm010@gmail.com>
2021-01-27 17:55:48 +0530

    updated deploy.sh with curl command

--- JenkinsScriptProject/deploy.sh
+++ JenkinsScriptProject/deploy.sh
@@ -2,5 +2,7 @@
 #	Deploy the war file to tomcat9
 #
 
+curl -v -u tomcat:tomcat -T **/*.war 'http://localhost:8081/manager/text/deploy?path=/deplyed-using-script-jenkins&update=true'
+
 #mvn deploy -DaltDeploymentRepository=script-repo::default::https://tomcat:tomcat@localhost:8081
-deploy adapters: [tomcat9(credentialsId: '6c41928c-4b3f-4672-ba81-43ccfe0d40e5', path: '', url: 'http://localhost:8081/')], contextPath: 'deployedfromscriptwebapp', war: '**/*.war'
+#deploy adapters: [tomcat9(credentialsId: '6c41928c-4b3f-4672-ba81-43ccfe0d40e5', path: '', url: 'http://localhost:8081/')], contextPath: 'deployedfromscriptwebapp', war: '**/*.war'
